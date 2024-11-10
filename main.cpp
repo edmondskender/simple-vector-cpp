dd53c90298a730aa23ddbf27433b0c271cfe3bf6
edmondskender <edmondskender.ai@gmail.com>
2024-11-10 15:34:13 +0100

    description

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 //
 // Created by edmond on 03.11.24.
 //
+/**
+ * @brief Dieses Programm berechnet alle Primzahlen bis zu einer vom Benutzer angegebenen Obergrenze n.
+ *
+ * Es verwendet die selbst implementierte Klasse `my_vector`, um die gefundenen Primzahlen zu speichern.
+ * Das Programm erwartet einen Kommandozeilenparameter, der die maximale Zahl n angibt.
+ * Nach der Berechnung werden alle gefundenen Primzahlen ausgegeben.
+ *
+ * Verwendung:
+ *   ./Programmname <n>
+ *
+ * Beispiel:
+ *   ./Primzahlrechner 100
+ *
+ * Dies berechnet alle Primzahlen bis einschließlich 100.
+ */
+
 #include "my_vector.h"
 #include <iostream>
 
